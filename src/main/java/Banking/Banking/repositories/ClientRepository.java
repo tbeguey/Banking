415e4c3baf47ecae863796c2d67eb4f0b5208f63
tbeguey <begueytheo@gmail.com>
2019-01-08 15:26:30 +0100

    Update ClientRepository.java

--- src/main/java/Banking/Banking/repositories/ClientRepository.java
+++ src/main/java/Banking/Banking/repositories/ClientRepository.java
@@ -1,12 +1,14 @@
 package Banking.Banking.repositories;
 
 import Banking.Banking.entities.Client;
-import Banking.Banking.entities.CompteCourant;
-import Banking.Banking.entities.Operation;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ClientRepository extends CrudRepository<Client, Integer> {
-    @Query("select cc from #{#entityName} as c inner join c.compteCourants as cc where cc.idCompteCourant = ?1 ")
-    Iterable<CompteCourant> findByCompteCourant(int id);
+    @Query(value="select c.* from client as c " +
+            "join compte_courant as cc on c.id_client = cc.id_client " +
+            "join compte_epargne as ce on c.id_client = ce.id_client " +
+            "where cc.solde > 2000 " +
+            "AND ce.solde > 10000", nativeQuery = true)
+    Iterable<Client> findRichClients();
 }
